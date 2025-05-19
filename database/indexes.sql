ddd4e5c6689f9ae2b2716ced7d5340152be45104
Paul Leflon <leflon.paul@gmail.com>
2025-05-19 10:30:06 +0200

    Update indexes

--- database/indexes.sql
+++ database/indexes.sql
@@ -1,1 +1,9 @@
-CREATE INDEX idx_BoardGames_basicinformation ON BoardGames (name, yearPublished, category);
+CREATE INDEX idx_BoardGames_name ON BoardGames (name);
+CREATE INDEX idx_BoardGames_category ON BoardGames (category);
+CREATE INDEX idx_BoardGames_price ON BoardGames (price);
+
+CREATE INDEX idx_user_email ON Users (email);
+
+CREATE INDEX idx_orders_userId ON Orders (userId);
+CREATE INDEX idx_carts_userId ON Carts (userId);
+CREATE INDEX idx_reviews_userId ON Reviews (userId);
